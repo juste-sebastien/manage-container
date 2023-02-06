0bb38653e995bcc4d96751092c903e9b5d03f8ab
Sébastien Juste <juste.sebastien@proton.me>
2023-02-06 15:14:46 +0100

    docs: add headers

---
+++ shell/1-principles/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#####################################################
+#
+# Description : deployement docker container
+#
+# Auteur : Sebastien J.
+#
+# Date : 06/02/2023
+#
+####################################################
+
+echo "
+Usage: 
+    * --create: run containers
+    * --drop: delete containers created by deploy.sh
+    * --infos: containers informations (ip, name, user...)
+    * --start: restart any container
+    * --ansible: deployement ansible tree
+"
+
