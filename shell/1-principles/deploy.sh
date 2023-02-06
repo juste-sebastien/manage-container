be109bf85929d714f5e1ea6eb0812f7a4d97d3a5
Sébastien Juste <juste.sebastien@proton.me>
2023-02-06 15:41:16 +0100

    feat: reading args

--- shell/1-principles/deploy.sh
+++ shell/1-principles/deploy.sh
@@ -10,12 +10,52 @@
 #
 ####################################################
 
-echo "
-Usage: 
-    * --create: run containers
-    * --drop: delete containers created by deploy.sh
-    * --infos: containers informations (ip, name, user...)
-    * --start: restart any container
-    * --ansible: deployement ansible tree
+
+# if option --create
+if [ "$1" == "--create" ];then
+  
+  echo ""
+  echo "--create run"
+  echo ""
+
+# if option drop
+elif [ "$1" == "--drop" ];then
+  
+  echo ""
+  echo "--drop run"
+  echo ""
+
+# if option start
+elif [ "$1" == "--start" ];then
+
+  echo ""
+  echo "--start run"
+  echo ""
+
+# if option info
+elif [ "$1" == "--info" ];then
+
+  echo ""
+  echo "--infos run"
+  echo ""
+
+# if option ansible
+elif [ "$1" == "--ansible" ];then
+
+  echo ""
+  echo "--ansible run"
+  echo ""
+
+# if not given option, display help
+else
+
+  echo "
+Usages:
+    - --create: run container
+    - --drop: delete container create by deploy.sh
+    - --info: get container information (ip, name, user...)
+    - --start: restart container
+    - --ansible: ansible tree deployment
 "
 
+fi
